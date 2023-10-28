4ecad590f10ddcc7843bcacaae2c66a7f60e04ec
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-10-28 21:59:44 +0900

    create raindrop
    비 모양 생성

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let rains = []
+
+function setup() {
+  createCanvas(400, 400);
+  for (let i = 0; i < 10; i = i + 1) {
+    let rain = new rain(radom(width),200);
+    rain.push(rains);
+  }
+}
+
+function draw() {
+  background(20);
+  for(let rain of rains){
+rain.
+rain.display()
+  }
+}
+
+class rain{
+  constructor(x,y){
+    this.pos = createVector(x,y);
+    this.len = random(15,30);
+  }
+
+  display(){
+    stroke(255);
+    line(this.pos.x, this.pos.y, this.pos.x, this.pos.y+this.len)
+  }
+}
