c8ee5b1d0d93d2c9aa0b8e670c075aafd4b459d8
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-10-28 22:25:08 +0900

    움직임 넣기

--- sketch.js
+++ sketch.js
@@ -10,19 +10,35 @@ function setup() {
 
 function draw() {
   background(20);
+let gravity = createVector(0, random(0.2,1));
+
   for(let rain of rains){
-rain.display()
+    rain.applyforce(gravity);
+    rain.update();
+rain.show()
   }
 }
 
 class Rain{
   constructor(x,y){
     this.pos = createVector(x,y);
+    this.vel = createVector();
+    this.acc = createVector();
     this.len = random(15,30);
+    this.thick = random(255);
   }
 
-  display(){
-    stroke(255);
+  applyforce(force){
+    this.acc.add(force)
+  }
+update(){
+  this.vel.add(this.acc);
+  this.pos.add(this.vel);
+
+  this.acc.mult(0);
+}
+  show(){
+    stroke(255,this.thick);
     line(this.pos.x, this.pos.y, this.pos.x, this.pos.y+this.len)
   }
 }
